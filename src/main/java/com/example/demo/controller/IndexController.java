49caed92a57f940ba927c2cdeffed98950d2b257
YG <9056>
2023-04-20 10:53:05 +0800

    elasticSearch demo

--- src/main/java/com/example/demo/controller/IndexController.java
+++ src/main/java/com/example/demo/controller/IndexController.java
@@ -9,6 +9,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class IndexController {
 
 
+    // TODO: 2023-4-20 遗留问题 1.通过controller访问静态页面，无法加载 
+    // TODO: 2023-4-20  2.引入的vue页面加载报错 
+    
   @GetMapping("/geth")
   @ResponseBody
     public  String index(){
